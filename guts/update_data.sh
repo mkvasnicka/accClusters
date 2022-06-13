b97ae1c18e88ed29dc39adbbe9b62024690783c0
Michal Kvasnička <prgosek@gmail.com>
2022-06-13 17:50:07 +0200

    guts: update_data.sh

--- guts/update_data.sh
+++ guts/update_data.sh
@@ -5,4 +5,4 @@ Rscript --vanilla $BASEDIR/prepare_districts.R
 Rscript --vanilla $BASEDIR/prepare_maps.R
 Rscript --vanilla $BASEDIR/prepare_points.R
 Rscript --vanilla $BASEDIR/prepare_hotspots.R
-# Rscript --vanilla
+# Rscript --vanilla $BASEDIR/prepare_clusters.R
