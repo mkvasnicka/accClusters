d514c7cc213cf95cbac8b2580f2caf8bb2fac037
Michal Kvasnička <prgosek@gmail.com>
2022-06-17 17:05:04 +0200

    guts: začátek řešení času u NKDE + odpovídající úpravy TODO

--- guts/functions_hotspots_preparation.R
+++ guts/functions_hotspots_preparation.R
@@ -17,6 +17,7 @@ require(tibble)
 compute_densities <- function(districts,
                               maps_dir, lixel_dir, sample_dir, accidents_dir,
                               density_dir,
+                              from_date, to_date,
                               weights = NULL, bw = 300,
                               adaptive = FALSE, trim_bw = 600,
                               method = "discontinuous", agg = 1,
@@ -24,15 +25,24 @@ compute_densities <- function(districts,
                               other_files = NULL) {
   one_district <- function(map_path, lixel_path, sample_path, accidents_path,
                            density_path,
+                           from_date, to_date,
                            weights, bw, adaptive, trim_bw, method, agg) {
     map <- readr::read_rds(map_path) |>
         sfnetworks::activate("edges") |>
         sf::st_as_sf()
     lixels <- readr::read_rds(lixel_path)
     samples <- readr::read_rds(sample_path)
-    accidents <- readr::read_rds(accidents_path)
+    accidents <- readr::read_rds(accidents_path) |>
+        dplyr::filter(p2a >= from_date, p2a <= to_date)
+    if (!(is.null(weights) ||
+          (is.character(weights) && length(weights) == 1 &&
+           weights %in% names(accidents) && is.numeric(accidents[[weights]]))))
+        stop("Weights must be eiter NULL or name of one numeric vector ",
+             "in accidents.")
     if (is.null(weights))
         weights <- rep(1,nrow(accidents))
+    if (is.character(weights))
+        weights <- accidents[[weights]]
     densities <- spNetwork::nkde(map,
                                  events = accidents,
                                  w = weights,
@@ -49,6 +59,10 @@ compute_densities <- function(districts,
     lixels$density <- densities
     write_dir_rds(lixels, density_path)
   }
+
+  from_date <- as.Date(from_date)
+  to_date <- as.Date(to_date)
+  stopifnot(from_date <= to_date)
 
   workers <- get_number_of_workers(workers)
   districts <- districts_behind(districts,
@@ -68,6 +82,7 @@ compute_densities <- function(districts,
       density_path = densities_file_name(districts, density_dir)
   )
   PWALK(tab, one_district, workers = get_number_of_workers(workers),
+        from_date, to_date,
         weights = weights, bw = bw, adaptive = adaptive, trim_bw = trim_bw,
         method = method, agg = agg)
 }
