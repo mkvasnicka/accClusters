2dc4e179b28d633e45e98eec39c887f5120ab18a
Michal Kvasnička <prgosek@gmail.com>
2022-06-24 13:28:54 +0200

    guts: update_data.sh je má nově obsah původního update_data_plus.sh

--- guts/update_data.sh
+++ guts/update_data.sh
@@ -1,9 +1,33 @@
 #!/usr/bin/bash
-set -e
+
+# this script updates data used in shiny visualization app
+# it works in waterfall way: it prepares districts, then it prepares maps for
+# all districts, and so on
+
+# you can pass parameters --profile=filename and workers=X where X is either
+# positive integer or auto
+
+# folder where R scripts are stored
 BASEDIR=guts
-Rscript --vanilla $BASEDIR/start_logging.R
-Rscript --vanilla $BASEDIR/prepare_districts.R
-Rscript --vanilla $BASEDIR/prepare_maps.R
-Rscript --vanilla $BASEDIR/prepare_points.R
-Rscript --vanilla $BASEDIR/prepare_hotspots.R
-# Rscript --vanilla $BASEDIR/prepare_clusters.R
+
+# command-line parameters
+CMLPARAMS=$@
+
+# stop on error
+set -e
+
+# function to run R scripts; it sets RSCRIPTDIR variable and handles other
+# parameters, too
+runRscript () {
+    Rscript --vanilla -e "RSCRIPTDIR=\"$BASEDIR\""\
+        -e "source(\"$BASEDIR/$1.R\")" "$CMLPARAMS"
+}
+
+runRscript "start_logging"
+runRscript "prepare_districts"
+runRscript "prepare_maps"
+runRscript "prepare_points"
+runRscript "prepare_hotspots"
+runRscript "prepare_clusters"
+
+# runRscript "test"
