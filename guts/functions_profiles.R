f5065cac2d169a6614ba94b802d36af29ff3b7cf
Michal Kvasnička <prgosek@gmail.com>
2022-08-29 14:27:35 +0200

    guts: změny v načítání profilů

--- guts/functions_profiles.R
+++ guts/functions_profiles.R
@@ -11,6 +11,7 @@
 
 library(purrr)
 library(stringr)
+library(rlang)
 
 
 # check_profile() checks that all necessary variables are present in a profile,
@@ -19,24 +20,22 @@ library(stringr)
 #
 # inputs:
 # - profile ... (environment)
+# - file_name ... (character scalar) path to the profile (just for messaging)
+# - necessary_slots, supported_slots ... (named lists) the first one names the
+#   variables that must be present in the profile, the second one names
+#   the variables that are allowed; names in the lists are
+#   the necessary/supported variable names, the their values define class and
+#   length expected
 #
 # value: none
-check_profile <- function(profile, file_name) {
+check_profile <- function(profile, file_name, necessary_slots, supported_slots) {
     # check that profile is environment
     stopifnot(is.environment(profile))
 
-    # definition of all supported slots
-    necessary_slots <- list(PROFILE_NAME = character(1))
-    supported_slots <- list(NKDE_WEIGHTS = character(1),
-                            NKDE_BW = double(1),
-                            NKDE_ADAPTIVE = logical(1),
-                            NKDE_TRIM_BW = double(1),
-                            NKDE_METHOD = character(1),
-                            NKDE_AGG = double(1))
     all_slots <- c(necessary_slots, supported_slots)
 
     necessary_names <- names(necessary_slots)
-    supported_names <- c(names(supported_slots), supported_names)
+    supported_names <- c(names(supported_slots), necessary_names)
     profile_names <- names(profile)
 
     # check all necessary names are present
@@ -65,18 +64,40 @@ check_profile <- function(profile, file_name) {
 }
 
 
-# read_profile() reads one R script that should include a profile; it evaluates
-# it in a new environment; then it checks it (see help for check_profile())
+# read_config_or_profile() reads one R script that should include a profile; it
+# evaluates it in a new environment
 #
 # inputs:
 # - profile ... (character scalar) path to profile R script
+# - e ... (environment) a profile on which this one is based-upon
 #
 # value: environment; it is a profile
-read_profile <- function(profile) {
-    e <- new.env()
+read_config_or_profile <- function(profile, based_on = new.env()) {
+    stopifnot(is.environment(based_on))
+    e <- rlang::env_clone(based_on)
     source(profile, local = e)
-    check_profile(e, file_name = profile)
     e
+}
+
+
+read_config <- function(config) {
+    e <- read_config_or_profile(config)
+}
+
+
+read_profile <- function(config, profile) {
+    # definition of all supported slots
+    necessary_slots <- list(PROFILE_NAME = character(1))
+    supported_slots <- list(NKDE_WEIGHTS = character(1),
+                            NKDE_BW = double(1),
+                            NKDE_ADAPTIVE = logical(1),
+                            NKDE_TRIM_BW = double(1),
+                            NKDE_METHOD = character(1),
+                            NKDE_AGG = double(1))
+
+    e <- read_config_or_profile(profile)
+    check_profile(e, profile, necessary_slots, supported_slots)
+    read_config_or_profile(profile, config)
 }
 
 
@@ -89,6 +110,19 @@ read_profile <- function(profile) {
 #
 # value: list of environments; each environment is one profile
 read_all_profiles <- function(folder) {
-    profiles <- list.files(folder, pattern = "\\.R", full.names = TRUE)
-    purrr::map(profiles, read_profile)
+    config <- read_config(file.path(folder, "config.R"))
+    profiles <- list.files(folder, pattern = "profile.*\\.R", full.names = TRUE)
+    profiles <- purrr::map(profiles, ~read_profile(config, .))
+    if (length(profiles) > 0) {
+        profile_names <- profiles |> purrr::map_chr("PROFILE_NAME")
+        if (any(duplicated(profile_names)))
+            stop("There are duplicities in profile names: ",
+                 stringr::str_flatten(profile_names[duplicated(profile_names)],
+                                      collapse = ", "))
+        names(profiles) <- profile_names
+    } else {
+        config$PROFILE_NAME <- "default"
+        profiles <- list(default = config)
+    }
+    profiles
 }
