2a6d842ba91a7b89ddf568786a09c86c2fe5b9a1
Michal Kvasnička <prgosek@gmail.com>
2022-09-05 14:52:38 +0200

    guts: add_damage_cost nově řeší NA v nákladech
    buď nahradí NA nulou, nebo vyhodí řádky s NA v nákladech

--- guts/functions_damage_cost.R
+++ guts/functions_damage_cost.R
@@ -19,19 +19,30 @@
 # - accident_serious_injury ... (integer) number of the seriously injured
 # - accident_light_injury ... (integer) number of the light injured
 # - accident_material_cost ... (double) material cost in mil. CZK
+# - na_zero ... (logical scalar) if TRUE (default), NAs in costs are replaced
+#   with 0s; if FALSE, all rows in accidents that have any NA cost are removed
 #
 # value:
 #   the same tibble as accidents but new column, accident cost, is added
 add_damage_cost <- function(accidents,
-                            unit_cost_dead, unit_cost_serious_injury,
-                            unit_cost_light_injury, unit_cost_material,
-                            unit_cost_const) {
-    accidents |>
+                            unit_cost_dead,
+                            unit_cost_serious_injury,
+                            unit_cost_light_injury,
+                            unit_cost_material,
+                            unit_cost_const, na_zero = TRUE) {
+    zero_na <- function(x, na_zero)
+        ifelse(is.na(x) & na_zero, 0, x)
+
+    accidents <- accidents |>
         mutate(accident_cost =
-                   accident_dead * unit_cost_dead +
-                   accident_serious_injury * unit_cost_serious_injury +
-                   accident_light_injury * unit_cost_light_injury +
-                   accident_material_cost * unit_cost_material +
+                   zero_na(accident_dead, na_zero) * unit_cost_dead +
+                   zero_na(accident_serious_injury, na_zero) * unit_cost_serious_injury +
+                   zero_na(accident_light_injury, na_zero) * unit_cost_light_injury +
+                   zero_na(accident_material_cost, na_zero) * unit_cost_material +
                    unit_cost_const
         )
+
+    if (!na_zero)
+        accidents <- accidents |> filter(!is.na(accident_cost))
+    accidents
 }
