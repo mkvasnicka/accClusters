c0b80a6e1d00c0d77edbd300c39f87dfac914050
Michal Kvasnička <prgosek@gmail.com>
2022-10-26 12:56:46 +0200

    core detection checks for docker limits

--- functions_auxiliary.R
+++ functions_auxiliary.R
@@ -498,18 +498,116 @@ districts_behind <- function(districts, target_fun, source_fun,
 
 # parallel processing ----------------------------------------------------------
 
+# docker_mem_limit() returns amount of memory available to the docker container
+# in which it's running
+#
+# inputs:
+#   none
+#
+# value:
+#   either available memory in GBs or Inf if unconstrained
+#
+# notes:
+# - it may be necessary to add a new file in mem_limit() in a new version
+#   of Docker
+docker_mem_limit <- function() {
+    mem_limit <- function(file) {
+        ml <- NA
+        if (file.exists(file)) {
+            ml <- base::readLines(file) |>
+                as.numeric() / 1024^3
+        }
+        ml
+    }
+
+    suppressWarnings(
+        c(
+            mem_limit("/sys/fs/cgroup/memory/memory.limit_in_bytes"),
+            mem_limit("/sys/fs/cgroup/memory.max")
+        ) |>
+            min(na.rm = TRUE)
+    )
+}
+
+
 # available_memory() returns memory available to the process at the moment in
 # GBs
-# TODO: add memory management inside a container
+#
+# inputs:
+#   none
+#
+# value:
+#   available memory in GBs
+#
+# notes:
+# - if run from inside a docker container, it returns the minimum of free memory
+#   at the moment and the size of the memory available to the container
+# - it returns the amount of free memory at the momenet otherwise
 available_memory <- function() {
-    as.numeric(memuse::Sys.meminfo()$freeram) / 1024 ^ 3
+    min(
+        docker_mem_limit(),
+        as.numeric(memuse::Sys.meminfo()$freeram) / 1024 ^ 3
+    )
+}
+
+
+# docker_cpu_limit() returns number of cpus available to the docker container
+# in which it's running
+#
+# inputs:
+#   none
+#
+# value:
+#   either number of cpus or Inf if unconstrained
+#
+# notes:
+# -
+# - it may be necessary to add a new file in cpu_limit() in a new version
+#   of Docker
+docker_cpu_limit <- function() {
+    cpu_limit <- function(file) {
+        cl <- NA
+        if (file.exists(file)) {
+            cl <- base::readLines(file) |>
+                base::strsplit("\\s+")
+            cl <- cl[[1]] |>
+                as.numeric() / 1e5
+            if (cl[2] != 1)
+                stop("I can't work with fractional cpu-periods.\n",
+                     "    Run docker with --cpu-period=100000!")
+        }
+        cl[1]
+    }
+
+    suppressWarnings(
+        c(
+            cpu_limit("/sys/fs/cgroup/cpu/cpu.cfs_quota_us"),
+            cpu_limit("/sys/fs/cgroup/cpu.max")
+        ) |>
+            min(na.rm = TRUE) |>
+            floor()
+    )
 }
 
 
 # available_cores() returns the number of cores available at the moment
-# TODO: add memory management inside a container
+#
+# inputs:
+#   none
+#
+# value:
+#   (integer scalar) number of available cpu cores
+#
+# notes:
+# - if run from inside a docker container, it returns the minimum of the number
+#   of cores dedicated to the docker container and the number of cores available
+#   at the machine
+# - it returns the number of cores available at the machine otherwise
 available_cores <- function() {
-    future::availableCores()
+    min(
+        docker_cpu_limit(),
+        future::availableCores()
+    ) |> as.integer()
 }
 
 
