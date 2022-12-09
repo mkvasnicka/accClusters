2e0644f5c0b50e53a868b2c1501c117e6d676111
Michal Kvasnička <prgosek@gmail.com>
2022-12-09 14:34:50 +0100

    PWALK now logs how many cores/workers it uses

--- functions_auxiliary.R
+++ functions_auxiliary.R
@@ -786,6 +786,8 @@ silently <- function(.f) {
 PWALK <- function(.l, .f, workers = 1, ram_needed = NULL, ...) {
     if (nrow(.l) > 0) {
         workers <- get_number_of_workers(workers, ram_needed)
+        logging::loginfo("PWALK: using %s worker%s", workers,
+                         if (workers > 1) "s in parallel" else " only")
         .f <- silently(.f)
         if (workers == 1) {
             success <- purrr::pmap(.l, .f, ...)
