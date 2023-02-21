a8c2b1556162b94ddff2da279921b5235c0a9fb0
Michal Kvasnička <prgosek@gmail.com>
2023-02-21 15:33:47 +0100

    default config correction

--- config/profile_default.R
+++ config/profile_default.R
@@ -15,26 +15,26 @@ PROFILE_COMMENT = "some string shown in shiny app"
 
 # damage costs -----------------------------------------------------------------
 
-# cost of each deceased persion in mil. CZK
+# cost of each deceased persion in mil. CZK (d1)
 # UNIT_COST_DEAD = 12
-# cost of each seriously injured person in mil. CZK
+# cost of each seriously injured person in mil. CZK (s1)
 # UNIT_COST_SERIOUS_INJURY = UNIT_COST_DEAD * 0.25
-# cost of each lightly injured person in mil. CZK
+# cost of each lightly injured person in mil. CZK (l1)
 # UNIT_COST_LIGHT_INJURY = UNIT_COST_DEAD * 0.003
-# material cost (in mil. CZK) is multiplied by the following coefficient
+# material cost (in mil. CZK) is multiplied by the following coefficient (m1)
 # UNIT_COST_MATERIAL = 1
-# the following coefficient (in mil. CZK) is added to each accidents' cost
-# UNIT_COST_CONST = 0
-# a fixed cost in mil. CZK added to accidents where someone lost her life
+# a fixed cost in mil. CZK added to accidents where someone lost her life (d2)
 # CONST_COST_DEAD = 0
 # a fixed cost in mil. CZK added to accidents where the worst damage was serious
-# injury
-# CONST_COST_SERIOUS_INJURY
+# injury (s2)
+# CONST_COST_SERIOUS_INJURY = 0
 # a fixed cost in mil. CZK added to accidents where the worst damage was light
-# injury
-# CONST_COST_LIGHT_INJURY
-# a fixed cost in mil. CZK added to accidents with material cost only
-# CONST_COST_MATERIAL
+# injury (l2)
+# CONST_COST_LIGHT_INJURY = 0
+# a fixed cost in mil. CZK added to accidents with material cost only (m2)
+# CONST_COST_MATERIAL = 0
+# the following coefficient (in mil. CZK) is added to each accidents' cost (c)
+# UNIT_COST_CONST = 0
 
 
 
